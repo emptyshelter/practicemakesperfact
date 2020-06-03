6e85de9f087ebba637da5002ce9d0bf8b25a5b8a
Sangsu <hiphopmy@naver.com>
2020-06-03 19:30:54 +0900

    ww

---
+++ spring-boot/src/main/java/co/kr/pmp/web/ss/WelcomeController.java
@@ -0,0 +1,44 @@
+package co.kr.pmp.web.ss;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ss")
+public class WelcomeController {	
+	public String generateState()
+	{
+	    SecureRandom random = new SecureRandom();
+	    return new BigInteger(130, random).toString(32);
+	}
+
+	
+	@RequestMapping("/welcome")
+	public String welcome(HttpSession session) {
+		
+		return "welcome";
+	}
+	
+	@RequestMapping("/test")
+	public String test() {
+		return "test";
+	}
+	
+	@RequestMapping("/success")
+	public String success() {
+		return "success";
+	}
+	
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "welcome";
+	}
+
+}
---
+++ spring-boot/src/main/webapp/WEB-INF/KSS/success.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Success</h1>
+<a href="/">처음으로</a>
+<a href="logout">로그아웃</a>
+</body>
+</html>
